83db2a4573be72534a82157c106487b58f9a32d3
ROHIT RANA <rohit.rana043@gmail.com>
2025-03-07 20:07:21 -0500

    navbar import fix

--- frontend/src/components/layout/UnsecureLayout.tsx
+++ frontend/src/components/layout/UnsecureLayout.tsx
@@ -1,10 +1,10 @@
 // src/components/layout/UnsecureLayout.tsx
 'use client';
 
-import Navbar from '@/components/layout/Navbar';
 import BaseLayout from './BaseLayout';
 import AnnouncementBanner from './AnnouncementBanner';
 import Footer from './Footer';
+import Navbar from './Navbar';
 
 export default function UnsecureLayout({
   children,
